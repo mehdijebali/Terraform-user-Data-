d426cf9f51759b7e85f44e0905090b266ae64e79
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-09-02 14:57:08 +0100

    update variables

--- UserData_Script/variables.tf
+++ UserData_Script/variables.tf
@@ -1,22 +1,9 @@
-variable "AWS_ACCESS_KEY" {
-    type = string
-    default = "AKIASMSIZOF42P2VUDSZ"
-}
-
-variable "AWS_SECRET_KEY" {}
-
 variable "AWS_REGION" {
-default = "us-east-2"
+default = "us-east-1"
 }
 
-variable "AMIS" {
-    type = map
-    default = {
-        us-east-1 = "ami-0f40c8f97004632f9"
-        us-east-2 = "ami-05692172625678b4e"
-        us-west-2 = "ami-0352d5a37fb4f603f"
-        us-west-1 = "ami-0f40c8f97004632f9"
-    }
+variable "AMI_ID" {
+  default = "ami-0b0ea68c435eb488d"
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
@@ -27,6 +14,6 @@ variable "PATH_TO_PUBLIC_KEY" {
   default = "levelup_key.pub"
 }
 
-variable "INSTANCE_USERNAME" {
-  default = "ubuntu"
+variable "INSTANCE_TYPE" {
+  default = "t2.micro"
 }
