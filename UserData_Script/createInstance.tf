42ee60d75016a445ed501334b7ae09e1f2cd2a8c
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-11-18 14:35:22 +0100

    Configure EC2 instance with security group

--- UserData_Script/createInstance.tf
+++ UserData_Script/createInstance.tf
@@ -8,6 +8,7 @@ resource "aws_key_pair" "levelup_key" {
 resource "aws_instance" "MyFirstInstnace" {
   ami           = var.AMI_ID
   instance_type = var.INSTANCE_TYPE
+  vpc_security_group_ids = [aws_security_group.allow-levelup-ssh.id]
   availability_zone = var.AVAILABILITY_ZONE
   key_name      = aws_key_pair.levelup_key.key_name
 
